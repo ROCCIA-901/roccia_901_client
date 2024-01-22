e87102ccf6436137e5bc6b1e967aa1c3cd4761e8
soonvro <ccsa3321@naver.com>
2024-01-22 19:27:29 +0900

    feat: add tmp_all_screen_list_screen.dart

---
+++ lib/screens/tmp_all_screen_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TmpAllScreenListScreen extends StatelessWidget {
+  const TmpAllScreenListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
