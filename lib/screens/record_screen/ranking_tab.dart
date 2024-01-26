1670513b786b6b904a8d064ff4ad75d6404f88d9
soonvro <ccsa3321@naver.com>
2024-01-26 14:33:39 +0900

    feat: add ranking criteria button

--- lib/screens/record_screen/ranking_tab.dart
+++ lib/screens/record_screen/ranking_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 enum RankingType {
@@ -14,17 +15,47 @@ class RankingTab extends StatefulWidget {
 }
 
 class _RankingTabState extends State<RankingTab> {
+  /// 주간, 전체 중 어느 타입의 랭킹을 보여줄지
   RankingType rakingType = RankingType.weekly;
+
+  /// 주간 랭킹에서 어느 주간을 보여줄지
+  late int rankingWeek;
+
+  /// 전체 랭킹에서 어느 기수를 보여줄지
+  late int rankingGeneration;
+
+  @override
+  void initState() {
+    super.initState();
+    rankingWeek = 1;
+    rankingGeneration = 10;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(width: double.maxFinite, height: 30),
+        SizedBox(width: double.maxFinite, height: 25),
+
+        /// 주간, 전체 랭킹 버튼 그룹
         RankingTypeButtonGroup(
           rankingType: rakingType,
           changeRankingType: changeRankingType,
         ),
+        SizedBox(width: double.maxFinite, height: 15),
+
+        /// 주차 및 기수 변경 위젯
+        RankingTurnGroup(
+          rankingType: rakingType,
+          rankingWeek: rankingWeek,
+          rankingGeneration: rankingGeneration,
+        ),
+
+        /// 랭킹 기준 안내 버튼
+        RankingCriteriaButton(),
+
+        /// 회색 가로선
+        Container(height: 1.5, color: Color(0xFFF1F1F1)),
       ],
     );
   }
@@ -94,19 +125,158 @@ class RankingTypeBotton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FilledButton(
-      style: FilledButton.styleFrom(
-        backgroundColor: Color(0xFFE0E0E0),
-        foregroundColor: Colors.white,
-        disabledBackgroundColor: Theme.of(context).colorScheme.primary,
-        disabledForegroundColor: Colors.white,
-        textStyle: GoogleFonts.inter(
-          fontSize: 18,
-          fontWeight: FontWeight.w600,
+    const double widthRatio = 5;
+    const double heightRatio = 2;
+    const double buttonScale = 17;
+    return SizedBox(
+      width: widthRatio * buttonScale,
+      height: heightRatio * buttonScale,
+      child: FilledButton(
+        style: FilledButton.styleFrom(
+          padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+          backgroundColor: Color(0xFFE0E0E0),
+          foregroundColor: Colors.white,
+          disabledBackgroundColor: Theme.of(context).colorScheme.primary,
+          disabledForegroundColor: Colors.white,
+          textStyle: GoogleFonts.inter(
+            fontSize: 15,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        onPressed: active ? onPressed : null,
+        child: Text(text),
+      ),
+    );
+  }
+}
+
+/// 주차 및 기수 변경 위젯
+class RankingTurnGroup extends StatelessWidget {
+  final RankingType rankingType;
+  final int rankingWeek;
+  final int rankingGeneration;
+
+  const RankingTurnGroup({
+    super.key,
+    required this.rankingType,
+    required this.rankingWeek,
+    required this.rankingGeneration,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // 현재 표시 중인 랭킹의 주차 또는 기수 정보
+    final String nowRanking = switch (rankingType) {
+      RankingType.weekly => '$rankingWeek주차',
+      RankingType.all => '$rankingGeneration기',
+    };
+    // arrow icon size
+    const double arrowIconSize = 18;
+
+    return SizedBox(
+      width: double.maxFinite,
+      height: 35,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(
+            padding: EdgeInsets.zero,
+            visualDensity: VisualDensity.compact,
+            onPressed: () {},
+            icon: Icon(
+              Icons.arrow_back_ios_new,
+              color: Color(0xFF7B7B7B),
+              size: arrowIconSize,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.zero,
+            padding: EdgeInsets.zero,
+            alignment: Alignment.center,
+            width: 240,
+            child: Text(
+              nowRanking,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF7B7B7B),
+              ),
+            ),
+          ),
+          IconButton(
+            padding: EdgeInsets.zero,
+            visualDensity: VisualDensity.compact,
+            onPressed: () {},
+            icon: Icon(
+              Icons.arrow_forward_ios,
+              color: Color(0xFF7B7B7B),
+              size: arrowIconSize,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// 랭킹 기준 안내 버튼
+class RankingCriteriaButton extends StatelessWidget {
+  const RankingCriteriaButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.zero,
+      padding: EdgeInsets.symmetric(vertical: 0, horizontal: 8),
+      alignment: Alignment.centerRight,
+      width: double.maxFinite,
+      height: 30,
+      child: TextButton.icon(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero,
+          alignment: Alignment.bottomCenter,
+        ),
+        onPressed: () {},
+        icon: SvgPicture.asset(
+          'assets/question_mark_circle.svg',
+          color: Color(0xFFE0E0E0),
+          width: 13,
+        ),
+        label: Text(
+          '랭킹 기준',
+          style: GoogleFonts.inter(
+            textStyle: TextStyle(
+              color: Color(0xFFE0E0E0),
+              fontSize: 13,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
         ),
       ),
-      onPressed: active ? onPressed : null,
-      child: Text(text),
+    );
+  }
+}
+
+class RankingListView extends StatelessWidget {
+  const RankingListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final PageController controller = PageController();
+    return PageView(
+      controller: controller,
+      children: const <Widget>[
+        Center(
+          child: Text('First Page'),
+        ),
+        Center(
+          child: Text('Second Page'),
+        ),
+        Center(
+          child: Text('Third Page'),
+        ),
+      ],
     );
   }
 }
