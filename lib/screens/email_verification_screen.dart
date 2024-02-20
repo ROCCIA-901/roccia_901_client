df98ff96926e81d64f1fb627743900a7098f9f6e
soonvro <soonvro@gmail.com>
2024-02-20 16:28:34 +0900

    feat: 인증번호 타임아웃 카운트 다운 기능 추가

--- lib/screens/email_verification_screen.dart
+++ lib/screens/email_verification_screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:untitled/config/app_constants.dart';
 
 import '../config/size_config.dart';
+import '../utils/countdown_timer.dart';
 import '../widgets/app_common_text_button.dart';
 
-class EmailVerificationScreen extends StatelessWidget {
+class EmailVerificationScreen extends StatefulWidget {
   const EmailVerificationScreen({super.key});
+
+  @override
+  State<EmailVerificationScreen> createState() =>
+      _EmailVerificationScreenState();
+}
+
+class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
+  /// 인증 타임아웃 카운트다운을 위한 변수
+  late int _currentCount;
+  late CountdownTimer _countdownTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentCount = AppConstants.authenticationTimeOut;
+    _countdownTimer = CountdownTimer(
+      duration: AppConstants.authenticationTimeOut,
+      onTick: decreaseCount,
+      onDone: () {},
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,24 +100,14 @@ class EmailVerificationScreen extends StatelessWidget {
                       ),
 
                       /// 인증번호 받기 버튼
-                      SizedBox(
-                        width: SizeConfig.safeBlockHorizontal * 27.78,
-                        child: AppCommonTextButton(
-                          text: Text(
-                            '인증번호 받기',
-                            style: GoogleFonts.inter(
-                              fontSize: SizeConfig.safeBlockHorizontal * 4.0,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFFFFFFFF),
-                            ),
-                          ),
-                          backgroundColor:
-                              Theme.of(context).colorScheme.primary,
-                          cornerRadius: 10,
-                          width: double.maxFinite,
-                          height: double.maxFinite,
-                          onPressed: () {},
-                        ),
+                      GetAuthenticationButton(
+                        firstOnPressed: _countdownTimer.start,
+                        secondOnPressed: () {
+                          setState(() {
+                            _currentCount = AppConstants.authenticationTimeOut;
+                          });
+                          _countdownTimer.restart();
+                        },
                       ),
                     ],
                   ),
@@ -116,32 +129,50 @@ class EmailVerificationScreen extends StatelessWidget {
                       /// 인증번호 입력 필드
                       SizedBox(
                         width: SizeConfig.safeBlockHorizontal * 55.56,
-                        child: TextField(
-                          keyboardType: TextInputType.number,
-                          textInputAction: TextInputAction.done,
-                          decoration: InputDecoration(
-                            labelText: '1:00',
-                            labelStyle: GoogleFonts.roboto(
-                              fontSize: SizeConfig.safeBlockHorizontal * 3.5,
-                              color: Color(0xFFD1D3D9),
-                            ),
-                            contentPadding: EdgeInsets.symmetric(
-                              horizontal: SizeConfig.safeBlockHorizontal * 3,
-                              vertical: 0,
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(6),
-                              borderSide: BorderSide(
-                                color: Color(0xFFD1D3D9),
+                        child: Stack(
+                          alignment: Alignment.topLeft,
+                          children: [
+                            TextField(
+                              keyboardType: TextInputType.number,
+                              textInputAction: TextInputAction.done,
+                              style: GoogleFonts.roboto(
+                                fontSize: SizeConfig.safeBlockHorizontal * 3.5,
+                                color: Colors.black,
+                              ),
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.symmetric(
+                                  horizontal:
+                                      SizeConfig.safeBlockHorizontal * 3,
+                                  vertical: 0,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(6),
+                                  borderSide: BorderSide(
+                                    color: Color(0xFFD1D3D9),
+                                  ),
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(6),
+                                  borderSide: BorderSide(
+                                    color: Color(0xFFD1D3D9),
+                                  ),
+                                ),
                               ),
                             ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(6),
-                              borderSide: BorderSide(
-                                color: Color(0xFFD1D3D9),
+                            // 카운트 다운 타이머
+                            Positioned(
+                              top: SizeConfig.safeBlockHorizontal * 3.2,
+                              right: SizeConfig.safeBlockHorizontal * 3.2,
+                              child: Text(
+                                "${(_currentCount / 60).floor()}:${(_currentCount % 60).toString().padLeft(2, '0')}",
+                                style: GoogleFonts.roboto(
+                                  fontSize:
+                                      SizeConfig.safeBlockHorizontal * 3.7,
+                                  color: Color(0xFFD1D3D9),
+                                ),
                               ),
                             ),
-                          ),
+                          ],
                         ),
                       ),
 
@@ -194,6 +225,15 @@ class EmailVerificationScreen extends StatelessWidget {
       ),
     );
   }
+
+  /// 카운트다운 초당 callback 함수
+  void decreaseCount() {
+    setState(() {
+      if (_currentCount > 0) {
+        _currentCount--;
+      }
+    });
+  }
 }
 
 /// 입력 정보 라벨 위젯
@@ -220,6 +260,56 @@ class InputLabel extends StatelessWidget {
           fontWeight: FontWeight.bold,
           color: Colors.black,
         ),
+      ),
+    );
+  }
+}
+
+/// 인증번호 받기 버튼
+/// 한번 누르면 onPressed 함수가 secondOnPressed로 바뀐다.
+class GetAuthenticationButton extends StatefulWidget {
+  const GetAuthenticationButton({
+    super.key,
+    required this.firstOnPressed,
+    required this.secondOnPressed,
+  });
+
+  final void Function() firstOnPressed;
+  final void Function() secondOnPressed;
+
+  @override
+  State<GetAuthenticationButton> createState() =>
+      _GetAuthenticationButtonState();
+}
+
+class _GetAuthenticationButtonState extends State<GetAuthenticationButton> {
+  bool _isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: SizeConfig.safeBlockHorizontal * 27.78,
+      child: AppCommonTextButton(
+        text: Text(
+          '인증번호 받기',
+          style: GoogleFonts.inter(
+            fontSize: SizeConfig.safeBlockHorizontal * 4.0,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFFFFFFFF),
+          ),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        cornerRadius: 10,
+        width: double.maxFinite,
+        height: double.maxFinite,
+        onPressed: () {
+          if (_isPressed) {
+            widget.secondOnPressed();
+          } else {
+            _isPressed = true;
+            widget.firstOnPressed();
+          }
+        },
       ),
     );
   }
--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:untitled/config/app_constants.dart';
+import 'package:untitled/utils/countdown_timer.dart';
 import 'package:untitled/widgets/app_common_text_button.dart';
 
 import '../config/size_config.dart';
+
+// Todo 드롭다운 메뉴 확장 시 화면 스크롤 안되는 문제 해결
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -45,6 +48,21 @@ class _SignUpScreenState extends State<SignUpScreen> {
   /// 선택된 프로필
   int? _profileIndex;
 
+  /// 인증 타임아웃 카운트다운을 위한 변수
+  late int _currentCount;
+  late CountdownTimer _countdownTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentCount = AppConstants.authenticationTimeOut;
+    _countdownTimer = CountdownTimer(
+      duration: AppConstants.authenticationTimeOut,
+      onTick: decreaseCount,
+      onDone: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,6 +91,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
             children: <Widget>[
               /// 이메일 label
               InputLabel(label: '이메일'),
+              // 이메일 입력 필드 & 인증번호 받기 버튼
               Container(
                 width: double.maxFinite,
                 margin: EdgeInsets.only(
@@ -122,29 +141,20 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       ),
 
                       /// 인증번호 받기 버튼
-                      SizedBox(
-                        width: SizeConfig.safeBlockHorizontal * 27.78,
-                        child: AppCommonTextButton(
-                          text: Text(
-                            '인증번호 받기',
-                            style: GoogleFonts.inter(
-                              fontSize: SizeConfig.safeBlockHorizontal * 4.0,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFFFFFFFF),
-                            ),
-                          ),
-                          backgroundColor:
-                              Theme.of(context).colorScheme.primary,
-                          cornerRadius: 10,
-                          width: double.maxFinite,
-                          height: double.maxFinite,
-                          onPressed: () {},
-                        ),
+                      GetAuthenticationButton(
+                        firstOnPressed: _countdownTimer.start,
+                        secondOnPressed: () {
+                          setState(() {
+                            _currentCount = AppConstants.authenticationTimeOut;
+                          });
+                          _countdownTimer.restart();
+                        },
                       ),
                     ],
                   ),
                 ),
               ),
+              // 인증 번호 입력 필드 & 인증번호 확인 버튼
               Container(
                 width: double.maxFinite,
                 alignment: Alignment.center,
@@ -156,37 +166,51 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       /// 인증번호 입력 필드
                       SizedBox(
                         width: SizeConfig.safeBlockHorizontal * 55.56,
-                        child: TextField(
-                          keyboardType: TextInputType.number,
-                          textInputAction: TextInputAction.done,
-                          style: GoogleFonts.roboto(
-                            fontSize: SizeConfig.safeBlockHorizontal * 3.5,
-                            color: Colors.black,
-                          ),
-                          decoration: InputDecoration(
-                            labelText: '1:00',
-                            labelStyle: GoogleFonts.roboto(
-                              fontSize: SizeConfig.safeBlockHorizontal * 3.5,
-                              color: Color(0xFFD1D3D9),
+                        child: Stack(
+                          alignment: Alignment.topLeft,
+                          children: [
+                            TextField(
+                              keyboardType: TextInputType.number,
+                              textInputAction: TextInputAction.done,
+                              style: GoogleFonts.roboto(
+                                fontSize: SizeConfig.safeBlockHorizontal * 3.5,
+                                color: Colors.black,
+                              ),
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.symmetric(
+                                  horizontal:
+                                      SizeConfig.safeBlockHorizontal * 3,
+                                  vertical: 0,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(6),
+                                  borderSide: BorderSide(
+                                    color: Color(0xFFD1D3D9),
+                                  ),
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(6),
+                                  borderSide: BorderSide(
+                                    color: Color(0xFFD1D3D9),
+                                  ),
+                                ),
+                              ),
+                              controller: _authenticationController,
                             ),
-                            contentPadding: EdgeInsets.symmetric(
-                              horizontal: SizeConfig.safeBlockHorizontal * 3,
-                              vertical: 0,
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(6),
-                              borderSide: BorderSide(
-                                color: Color(0xFFD1D3D9),
+                            // 카운트 다운 타이머
+                            Positioned(
+                              top: SizeConfig.safeBlockHorizontal * 3.2,
+                              right: SizeConfig.safeBlockHorizontal * 3.2,
+                              child: Text(
+                                "${(_currentCount / 60).floor()}:${(_currentCount % 60).toString().padLeft(2, '0')}",
+                                style: GoogleFonts.roboto(
+                                  fontSize:
+                                      SizeConfig.safeBlockHorizontal * 3.5,
+                                  color: Color(0xFFD1D3D9),
+                                ),
                               ),
                             ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(6),
-                              borderSide: BorderSide(
-                                color: Color(0xFFD1D3D9),
-                              ),
-                            ),
-                          ),
-                          controller: _authenticationController,
+                          ],
                         ),
                       ),
 
@@ -501,6 +525,15 @@ class _SignUpScreenState extends State<SignUpScreen> {
       _profileIndex = index;
     });
   }
+
+  /// 카운트다운 초당 callback 함수
+  void decreaseCount() {
+    setState(() {
+      if (_currentCount > 0) {
+        _currentCount--;
+      }
+    });
+  }
 }
 
 /// 입력 정보 라벨 위젯
@@ -527,6 +560,56 @@ class InputLabel extends StatelessWidget {
           fontWeight: FontWeight.bold,
           color: Colors.black,
         ),
+      ),
+    );
+  }
+}
+
+/// 인증번호 받기 버튼
+/// 한번 누르면 onPressed 함수가 secondOnPressed로 바뀐다.
+class GetAuthenticationButton extends StatefulWidget {
+  const GetAuthenticationButton({
+    super.key,
+    required this.firstOnPressed,
+    required this.secondOnPressed,
+  });
+
+  final void Function() firstOnPressed;
+  final void Function() secondOnPressed;
+
+  @override
+  State<GetAuthenticationButton> createState() =>
+      _GetAuthenticationButtonState();
+}
+
+class _GetAuthenticationButtonState extends State<GetAuthenticationButton> {
+  bool _isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: SizeConfig.safeBlockHorizontal * 27.78,
+      child: AppCommonTextButton(
+        text: Text(
+          '인증번호 받기',
+          style: GoogleFonts.inter(
+            fontSize: SizeConfig.safeBlockHorizontal * 4.0,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFFFFFFFF),
+          ),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        cornerRadius: 10,
+        width: double.maxFinite,
+        height: double.maxFinite,
+        onPressed: () {
+          if (_isPressed) {
+            widget.secondOnPressed();
+          } else {
+            _isPressed = true;
+            widget.firstOnPressed();
+          }
+        },
       ),
     );
   }
