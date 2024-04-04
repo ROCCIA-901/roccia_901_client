c2fceab35295d87cc819d2f5320c38f5c4e72fdf
soonvro <soonvro@gmail.com>
2024-04-04 22:03:10 +0900

    refactor: Riverpod를 이용한 singleton으로 변경

--- lib/data/shared/app_storage.dart
+++ lib/data/shared/app_storage.dart
@@ -1,9 +1,28 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-FlutterSecureStorage get appStorage => SecureStorage.instance;
-
-class SecureStorage extends FlutterSecureStorage {
-  SecureStorage._() : super();
-
-  static final instance = SecureStorage._();
+abstract interface class IAppStorage {
+  Future<void> write(String key, String value);
+  Future<String?> read(String key);
+  Future<void> delete(String key);
 }
+
+class AppStorage implements IAppStorage {
+  final FlutterSecureStorage _storage;
+
+  AppStorage(this._storage);
+
+  @override
+  Future<void> write(String key, String value) =>
+      _storage.write(key: key, value: value);
+
+  @override
+  Future<String?> read(String key) => _storage.read(key: key);
+
+  @override
+  Future<void> delete(String key) => _storage.delete(key: key);
+}
+
+final appStorageProvider = Provider<IAppStorage>(
+  (ref) => AppStorage(FlutterSecureStorage()),
+);
