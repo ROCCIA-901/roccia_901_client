d461628bdcb1c7ded1b2a652ad0673308ad00c39
soonvro <soonvro@gmail.com>
2024-04-08 14:12:56 +0900

    refactor: enum types 분리

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  /// Api request timeout
+  static const int apiRequestTimeout = 10;
+
+  /// 최신 기수 값
+  static const int maxGeneration = 11;
+
+  /// 인증 번호 타임 아웃 시간(sec)
+  static const int authenticationTimeOut = 60;
+}
