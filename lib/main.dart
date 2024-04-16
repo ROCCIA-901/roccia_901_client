77dedb578022d22349cf871d32b1c16932e5a602
soonvro <soonvro@gmail.com>
2024-04-16 23:09:42 +0900

    refactor: 이동한 파일 import 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:untitled/screens/tmp_all_screen_list_screen.dart';
-import 'package:untitled/utils/app_routes.dart';
-import 'package:untitled/utils/app_theme.dart';
-import 'package:untitled/constants/size_config.dart';
-import 'package:untitled/widgets/app_navigation_bar.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-import 'screens/member_home_screen.dart';
-import 'screens/record_screen/my_record_tab.dart';
-import 'screens/competition_screen.dart';
-import 'screens/my_page_screen.dart';
+import 'constants/size_config.dart';
+import 'presentation/screens/tmp_all_screen_list_screen.dart';
+import 'utils/app_routes.dart';
+import 'utils/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,7 +22,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SizeConfig().init(context);
+    SizeConfig.init(context);
     return MaterialApp(
       builder: (context, child) {
         return MediaQuery(
@@ -46,31 +41,31 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Roccia extends StatefulWidget {
-  @override
-  _RocciaState createState() => _RocciaState();
-}
-
-class _RocciaState extends State<Roccia> {
-  int currentIndex = 0;
-
-  final screens = [
-    MemberHomeScreen(),
-    MyRecordTab(),
-    CompetitionScreen(),
-    MyPageScreen(),
-  ];
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: screens[currentIndex],
-      bottomNavigationBar: AppNavigationBar(
-        currentIndex: currentIndex,
-        onItemSelected: (index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-      ),
-    );
-  }
-}
+// class Roccia extends StatefulWidget {
+//   @override
+//   _RocciaState createState() => _RocciaState();
+// }
+//
+// class _RocciaState extends State<Roccia> {
+//   int currentIndex = 0;
+//
+//   final screens = [
+//     MemberHomeScreen(),
+//     MyRecordTab(),
+//     CompetitionScreen(),
+//     MyPageScreen(),
+//   ];
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: screens[currentIndex],
+//       bottomNavigationBar: AppNavigationBar(
+//         currentIndex: currentIndex,
+//         onItemSelected: (index) {
+//           setState(() {
+//             currentIndex = index;
+//           });
+//         },
+//       ),
+//     );
+//   }
+// }
