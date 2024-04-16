8cc93135d3dccd7a5cc72e14710a6239ad8968a8
soonvro <soonvro@gmail.com>
2024-04-16 22:48:52 +0900

    feat: AppStorage 예외 처리 및 Provider 생성

--- lib/data/shared/app_storage.dart
+++ lib/data/shared/app_storage.dart
@@ -1,10 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:untitled/utils/app_logger.dart';
+
+final appStorageProvider = Provider<IAppStorage>(
+  (ref) => AppStorage(FlutterSecureStorage()),
+);
 
 abstract interface class IAppStorage {
-  Future<void> write(String key, String value);
-  Future<String?> read(String key);
-  Future<void> delete(String key);
+  Future<void> write({required String key, required String value});
+  Future<String?> read({required String key});
+  Future<void> delete({required String key});
 }
 
 class AppStorage implements IAppStorage {
@@ -13,16 +18,34 @@ class AppStorage implements IAppStorage {
   AppStorage(this._storage);
 
   @override
-  Future<void> write(String key, String value) =>
-      _storage.write(key: key, value: value);
+  Future<void> write({required String key, required String value}) async {
+    try {
+      await _storage.write(key: key, value: value);
+    } catch (e) {
+      throw _errorHandler(e);
+    }
+  }
 
   @override
-  Future<String?> read(String key) => _storage.read(key: key);
+  Future<String?> read({required String key}) async {
+    try {
+      return await _storage.read(key: key);
+    } catch (e) {
+      throw _errorHandler(e);
+    }
+  }
 
   @override
-  Future<void> delete(String key) => _storage.delete(key: key);
+  Future<void> delete({required String key}) async {
+    try {
+      await _storage.delete(key: key);
+    } catch (e) {
+      throw _errorHandler(e);
+    }
+  }
+
+  Exception _errorHandler(Object e) {
+    logger.w("App Storage Error: ${e.toString()}");
+    return e as Exception;
+  }
 }
-
-final appStorageProvider = Provider<IAppStorage>(
-  (ref) => AppStorage(FlutterSecureStorage()),
-);
