227b74fce8dc56be509f9646ef67cb10231a2912
soonvro <soonvro@gmail.com>
2024-04-16 22:41:38 +0900

    feat: add constants

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -5,6 +5,9 @@ class AppConstants {
   /// 최신 기수 값
   static const int maxGeneration = 11;
 
+  /// 정기 운동 지점 개수
+  static const int workoutLocationCount = 5;
+
   /// 인증 번호 타임 아웃 시간(sec)
-  static const int authenticationTimeOut = 60;
+  static const int authenticationTimeOut = 600;
 }
