c0b908c5b702c02159822c067d27fbb25993fab6
soonvro <soonvro@gmail.com>
2024-04-18 22:03:35 +0900

    constant: AppColors Constant 추가

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFCAE4C1);
+  static const Color yellow = Color(0xFFFFF8C1);
+  static const Color grayLight = Color(0xFFE0E0E0);
+  static const Color grayMedium = Color(0xFF7B7B7B);
+}
