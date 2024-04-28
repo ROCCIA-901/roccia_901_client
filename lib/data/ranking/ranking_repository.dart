e8349c04893add04f00f22fefc062b6cfe0e02ae
soonvro <soonvro@gmail.com>
2024-04-28 22:22:16 +0900

    feat: 랭킹 Repository 추가

---
+++ lib/data/ranking/ranking_repository.dart
@@ -0,0 +1,69 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:untitled/domain/ranking/rankings.dart';
+
+import '../../constants/roccia_api.dart';
+import '../../utils/app_logger.dart';
+import '../authentication/token_repository.dart';
+import '../shared/api_exception.dart';
+import '../shared/roccia_api_client.dart';
+
+final rankingRepositoryProvider = Provider<RankingRepository>(
+  (ref) => RankingRepository(
+    api: RocciaApi(),
+    apiClient: RocciaApiClient(
+      interceptors: [
+        RocciaApiAuthInterceptor(ref.read(tokenRepositoryProvider)),
+        RocciaApiLoggerInterceptor(),
+      ],
+      retryPolicy: ExpiredTokenRetryPolicy(
+        RocciaApi(),
+        ref.read(tokenRepositoryProvider),
+      ),
+    ),
+  ),
+);
+
+class RankingRepository {
+  final RocciaApi _api;
+  final RocciaApiClient _apiClient;
+
+  RankingRepository({
+    required RocciaApi api,
+    required RocciaApiClient apiClient,
+  })  : _api = api,
+        _apiClient = apiClient;
+
+  Future<WeeklyRankings> fetchWeeklyRankings() async {
+    try {
+      final uri = _api.ranking.weekly();
+      final body = await _apiClient.get(uri);
+      return fromJsonWeeklyRankings(body);
+    } catch (e, stackTrace) {
+      _errorHandler(e, stackTrace);
+      rethrow;
+    }
+  }
+
+  Future<GenerationRankings> fetchGenerationRankings() async {
+    try {
+      final uri = _api.ranking.generations();
+      final body = await _apiClient.get(uri);
+      return fromJsonGenerationRankings(body);
+    } catch (e, stackTrace) {
+      _errorHandler(e, stackTrace);
+      rethrow;
+    }
+  }
+
+  void _errorHandler(e, StackTrace stackTrace) {
+    try {
+      throw e;
+    } on ApiException catch (_) {
+      rethrow;
+    } catch (e) {
+      logger.w('On Ranking Repo: ${e.toString()}',
+          stackTrace: stackTrace, error: e);
+      rethrow;
+    }
+  }
+}
