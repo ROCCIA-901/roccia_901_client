42eaaf735dc52f87c5b49fc2fea9ab294033d361
soonvro <soonvro@gmail.com>
2024-04-28 22:04:36 +0900

    feat: app color 추가

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -6,4 +6,7 @@ class AppColors {
   static const Color grayLight = Color(0xFFF1F1F1);
   static const Color grayMedium = Color(0xFFE0E0E0);
   static const Color grayDark = Color(0xFF7B7B7B);
+  static const Color grayBorder = Color(0xFFD1D3D9);
+  static const Color redLight = Color(0XFFFFABB8);
+  static const Color redMedium = Color(0XFFEA7373);
 }
