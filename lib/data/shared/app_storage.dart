39fda1924d8e77f9c864de8e11349190f80c1171
soonvro <soonvro@gmail.com>
2024-04-28 22:06:40 +0900

    refactor: AppStorag class명 변경

--- lib/data/shared/app_storage.dart
+++ lib/data/shared/app_storage.dart
@@ -3,13 +3,51 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:untitled/utils/app_logger.dart';
 
 final appStorageProvider = Provider<IAppStorage>(
-  (ref) => AppStorage(FlutterSecureStorage()),
+  (ref) => AppSecureStorage(FlutterSecureStorage()),
 );
 
 abstract interface class IAppStorage {
   Future<void> write({required String key, required String value});
   Future<String?> read({required String key});
   Future<void> delete({required String key});
+}
+
+class AppSecureStorage implements IAppStorage {
+  final FlutterSecureStorage _storage;
+
+  AppSecureStorage(this._storage);
+
+  @override
+  Future<void> write({required String key, required String value}) async {
+    try {
+      await _storage.write(key: key, value: value);
+    } catch (e) {
+      throw _errorHandler(e);
+    }
+  }
+
+  @override
+  Future<String?> read({required String key}) async {
+    try {
+      return await _storage.read(key: key);
+    } catch (e) {
+      throw _errorHandler(e);
+    }
+  }
+
+  @override
+  Future<void> delete({required String key}) async {
+    try {
+      await _storage.delete(key: key);
+    } catch (e) {
+      throw _errorHandler(e);
+    }
+  }
+
+  Exception _errorHandler(Object e) {
+    logger.w("App Storage Error: ${e.toString()}");
+    return e as Exception;
+  }
 }
 
 class AppStorage implements IAppStorage {
