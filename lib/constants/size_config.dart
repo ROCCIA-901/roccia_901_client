d2678fecdd160658bfb5691198d749e76312d2a5
soonvro <soonvro@gmail.com>
2024-05-01 13:55:27 +0900

    feat: fontSize 추가

--- lib/constants/size_config.dart
+++ lib/constants/size_config.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SizeConfig {
+  static const double _maxMobileWidth = 768;
+
   static late MediaQueryData _mediaQueryData;
   static late double screenWidth;
   static late double screenHeight;
@@ -11,6 +15,8 @@ class SizeConfig {
   static late double _safeAreaVertical;
   static late double safeBlockHorizontal;
   static late double safeBlockVertical;
+
+  static late AppFontSize font;
 
   static void init(BuildContext context) {
     _mediaQueryData = MediaQuery.of(context);
@@ -23,7 +29,34 @@ class SizeConfig {
         _mediaQueryData.padding.left + _mediaQueryData.padding.right;
     _safeAreaVertical =
         _mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
-    safeBlockHorizontal = (screenWidth - _safeAreaHorizontal) / 100;
-    safeBlockVertical = (screenHeight - _safeAreaVertical) / 100;
+
+    // Limit the safe width to a maximum of mobile width
+    final double safeHeight = screenHeight - _safeAreaVertical;
+    double safeWidth =
+        min((screenWidth - _safeAreaHorizontal), _maxMobileWidth);
+    safeWidth = min(safeWidth, safeHeight * 0.7);
+
+    safeBlockHorizontal = safeWidth / 100;
+    safeBlockVertical = safeHeight / 100;
+
+    font = AppFontSize(safeBlockHorizontal);
+  }
+}
+
+class AppFontSize {
+  final double _safeBlockHorizontal;
+
+  AppFontSize(this._safeBlockHorizontal) {
+    setFontSizes();
+  }
+
+  late double headline1;
+  late double headline2;
+  late double headline3;
+
+  void setFontSizes() {
+    headline1 = _safeBlockHorizontal * 5.5;
+    headline2 = _safeBlockHorizontal * 4;
+    headline3 = _safeBlockHorizontal * 3.5;
   }
 }
