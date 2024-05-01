c5e2e5edd664f61d359ac50e83cdbfdb58c8e926
soonvro <soonvro@gmail.com>
2024-05-01 13:47:58 +0900

    design: 디자인 수정

--- lib/presentation/screens/member_home_screen.dart
+++ lib/presentation/screens/member_home_screen.dart
@@ -1,11 +1,17 @@
+import 'package:auto_route/annotations.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:untitled/constants/app_constants.dart';
+import 'package:untitled/utils/toast_helper.dart';
 import 'package:untitled/widgets/app_calendar.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../constants/size_config.dart';
 import '../viewmodels/attendance/attendance_dates_viewmodel.dart';
 
+@RoutePage()
 class MemberHomeScreen extends ConsumerStatefulWidget {
   const MemberHomeScreen({Key? key}) : super(key: key);
 
@@ -15,6 +21,10 @@ class MemberHomeScreen extends ConsumerStatefulWidget {
 
 class _MemberHomeState extends ConsumerState<MemberHomeScreen> {
   DateTime today = DateTime.now();
+
+  /// Configurations
+  final double _calendarWidth = SizeConfig.safeBlockHorizontal * 90;
+  late final double _calendarHeight = _calendarWidth * 0.85;
 
   @override
   void initState() {
@@ -27,33 +37,30 @@ class _MemberHomeState extends ConsumerState<MemberHomeScreen> {
   Widget build(BuildContext context) {
     var state = ref.watch(attendanceDatesViewModelProvider);
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '홈',
+          style: TextStyle(
+            fontSize: SizeConfig.font.headline1,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        surfaceTintColor: Colors.white,
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
             padding: EdgeInsets.only(
               left: SizeConfig.safeBlockHorizontal * 6.667,
               right: SizeConfig.safeBlockHorizontal * 6.667,
-              top: SizeConfig.safeBlockVertical * 3,
+              top: SizeConfig.safeBlockVertical * 2,
             ),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  padding: EdgeInsets.only(
-                    bottom: SizeConfig.safeBlockVertical * 4,
-                  ),
-                  child: Text(
-                    '홈',
-                    style: TextStyle(
-                      fontSize: SizeConfig.safeBlockHorizontal * 5,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                SvgPicture.asset(
-                  'assets/banners/howto.svg',
-                  width: SizeConfig.safeBlockHorizontal * 100,
-                ),
+                _buildBannerSlider(),
                 Container(
                   padding: EdgeInsets.only(
                     top: SizeConfig.safeBlockVertical * 4,
@@ -62,47 +69,12 @@ class _MemberHomeState extends ConsumerState<MemberHomeScreen> {
                   child: Text(
                     '출석',
                     style: TextStyle(
-                      fontSize: SizeConfig.safeBlockHorizontal * 3.3,
+                      fontSize: SizeConfig.font.headline2,
                       color: Color(0xff9a9a9a),
                     ),
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(
-                        right: SizeConfig.safeBlockHorizontal * 3,
-                      ),
-                      child: InkWell(
-                        child: SvgPicture.asset(
-                          'assets/buttons/goto_attendance_request_button.svg',
-                          width: SizeConfig.safeBlockHorizontal * 41,
-                        ),
-                        onTap: () {
-                          // Navigator.push(
-                          //     context,
-                          //     MaterialPageRoute(
-                          //         builder: (context) =>
-                          //             AttendanceRequestScreen()));
-                        },
-                      ),
-                    ),
-                    InkWell(
-                      child: SvgPicture.asset(
-                        'assets/buttons/goto_attendance_history_button.svg',
-                        width: SizeConfig.safeBlockHorizontal * 41,
-                      ),
-                      onTap: () {
-                        // Navigator.push(
-                        //     context,
-                        //     MaterialPageRoute(
-                        //         builder: (context) =>
-                        //             AttendanceHistoryScreen()));
-                      },
-                    )
-                  ],
-                ),
+                _buildAttendanceButtons(),
                 Container(
                   padding: EdgeInsets.only(
                     top: SizeConfig.safeBlockVertical * 4,
@@ -110,24 +82,83 @@ class _MemberHomeState extends ConsumerState<MemberHomeScreen> {
                   child: Text(
                     '출석 현황',
                     style: TextStyle(
-                      fontSize: SizeConfig.safeBlockHorizontal * 3.3,
+                      fontSize: SizeConfig.font.headline2,
                       color: Color(0xff9a9a9a),
                     ),
                   ),
                 ),
                 AppCalendar(
-                  width: SizeConfig.safeBlockHorizontal * 100,
-                  height: SizeConfig.safeBlockVertical * 40,
-                  eventsSource: switch (state) {
-                    AsyncData(:final value) => value.eventsSource,
-                    _ => {},
-                  },
+                  width: _calendarWidth,
+                  height: _calendarHeight,
+                  // eventsSource: switch (state) {
+                  //   AsyncData(:final value) => value.eventsSource,
+                  //   _ => {},
+                  // },
+                ),
+                SizedBox(
+                  height: SizeConfig.safeBlockVertical * 5,
                 ),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildBannerSlider() {
+    return CarouselSlider(
+      options: CarouselOptions(
+        autoPlay: true,
+        autoPlayInterval: Duration(seconds: 4),
+        autoPlayAnimationDuration: Duration(milliseconds: 800),
+        viewportFraction: 1,
+        height: SizeConfig.safeBlockHorizontal * 23,
+      ),
+      items: AppConstants.banners.map((banner) {
+        return Builder(
+          builder: (BuildContext context) {
+            return InkWell(
+              onTap: () async => await launchUrl(Uri.parse(banner.url)),
+              child: SvgPicture.asset(
+                banner.asset,
+                width: SizeConfig.safeBlockHorizontal * 100,
+              ),
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+
+  Widget _buildAttendanceButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(
+            right: SizeConfig.safeBlockHorizontal * 3,
+          ),
+          child: InkWell(
+            child: SvgPicture.asset(
+              'assets/buttons/goto_attendance_request_button.svg',
+              width: SizeConfig.safeBlockHorizontal * 41,
+            ),
+            onTap: () {
+              ToastHelper.showUnimplemented(context);
+            },
+          ),
+        ),
+        InkWell(
+          child: SvgPicture.asset(
+            'assets/buttons/goto_attendance_history_button.svg',
+            width: SizeConfig.safeBlockHorizontal * 41,
+          ),
+          onTap: () {
+            ToastHelper.showUnimplemented(context);
+          },
+        )
+      ],
     );
   }
 }
