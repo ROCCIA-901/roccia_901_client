63903e1d5a9a81e7f16de5865709c8833c264e15
soonvro <soonvro@gmail.com>
2024-05-01 13:47:26 +0900

    design: 디자인 수정

--- lib/widgets/loader_dialog.dart
+++ lib/widgets/loader_dialog.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../constants/size_config.dart';
+
 class LoaderDialog extends StatelessWidget {
   const LoaderDialog({super.key});
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      content: Row(
-        children: [
-          CircularProgressIndicator(),
-          Container(
-            margin: EdgeInsets.only(left: 7),
-            child: Text("Loading..."),
-          ),
-        ],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      content: Container(
+        width: SizeConfig.safeBlockHorizontal * 70,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(),
+            Container(
+              margin: EdgeInsets.only(left: 7),
+              child: Text("Loading..."),
+            ),
+          ],
+        ),
       ),
     );
   }
