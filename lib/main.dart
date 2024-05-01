13f5f629c58253cd9be7d7958f46d1e9f95a54b5
soonvro <soonvro@gmail.com>
2024-05-01 13:57:58 +0900

    feat: Web 실행 Widget 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,47 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 import 'constants/size_config.dart';
-import 'presentation/screens/tmp_all_screen_list_screen.dart';
-import 'utils/app_routes.dart';
+import 'utils/app_router.dart';
 import 'utils/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('ko'); // Initialize for default locale
   runApp(
-    ProviderScope(
-      child: MyApp(),
-    ),
+    ProviderScope(child: Builder(
+      builder: (context) {
+        SizeConfig.init(context);
+        return MyWebApp();
+      },
+    )),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyWebApp extends StatelessWidget {
+  MyWebApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    SizeConfig.init(context);
-    return MaterialApp(
+    return Center(
+      child: ClipRect(
+        child: SizedBox(
+          width: SizeConfig.safeBlockHorizontal * 100,
+          child: MyApp(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends ConsumerWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      routerConfig: AppRouter(ref).config(),
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
       builder: (context, child) {
         return MediaQuery(
           data: MediaQuery.of(context).copyWith(
@@ -32,40 +52,6 @@ class MyApp extends StatelessWidget {
           child: child!,
         );
       },
-      debugShowCheckedModeBanner: false,
-      title: 'bottomNavigationBar',
-      theme: AppTheme.lightTheme,
-      home: TmpAllScreenListScreen(),
-      routes: AppRoutes.routes,
     );
   }
 }
-
-// class Roccia extends StatefulWidget {
-//   @override
-//   _RocciaState createState() => _RocciaState();
-// }
-//
-// class _RocciaState extends State<Roccia> {
-//   int currentIndex = 0;
-//
-//   final screens = [
-//     MemberHomeScreen(),
-//     MyRecordTab(),
-//     CompetitionScreen(),
-//     MyPageScreen(),
-//   ];
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: screens[currentIndex],
-//       bottomNavigationBar: AppNavigationBar(
-//         currentIndex: currentIndex,
-//         onItemSelected: (index) {
-//           setState(() {
-//             currentIndex = index;
-//           });
-//         },
-//       ),
-//     );
-//   }
-// }
