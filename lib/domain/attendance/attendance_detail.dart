ff018fc251e4f9d153de636ed531ebf68d69d582
soonvro <soonvro@gmail.com>
2024-08-30 20:20:41 +0900

    feat: Attendance Detail 모델 추가

---
+++ lib/domain/attendance/attendance_detail.dart
@@ -0,0 +1,77 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import '../../constants/app_enum.dart';
+
+part 'attendance_detail.g.dart';
+
+@JsonSerializable(checked: true, fieldRename: FieldRename.snake)
+class AttendanceDetail {
+  @JsonKey()
+  final AttendanceDetailCount count;
+  @JsonKey()
+  final List<AttendanceDetailVerbose> detail;
+
+  AttendanceDetail({
+    required this.count,
+    required this.detail,
+  });
+
+  // ------------------------------------------------------------------------ //
+  //                      JSON SERIALIZATION                                  //
+  // ------------------------------------------------------------------------ //
+  factory AttendanceDetail.fromJson(Map<String, dynamic> json) => _$AttendanceDetailFromJson(json);
+}
+
+@JsonSerializable(
+  checked: true,
+  fieldRename: FieldRename.snake,
+  explicitToJson: true,
+)
+class AttendanceDetailCount {
+  @JsonKey()
+  final int attendance;
+  @JsonKey()
+  final int late;
+  @JsonKey()
+  final int absence;
+  @JsonKey()
+  final int alternative;
+
+  AttendanceDetailCount({
+    required this.attendance,
+    required this.late,
+    required this.absence,
+    required this.alternative,
+  });
+
+  // ------------------------------------------------------------------------ //
+  //                      JSON SERIALIZATION                                  //
+  // ------------------------------------------------------------------------ //
+  factory AttendanceDetailCount.fromJson(Map<String, dynamic> json) => _$AttendanceDetailCountFromJson(json);
+}
+
+@JsonSerializable(
+  checked: true,
+  fieldRename: FieldRename.snake,
+  explicitToJson: true,
+)
+class AttendanceDetailVerbose {
+  final int week;
+  Location workoutLocation;
+  String attendanceStatus;
+  String requestDate;
+  String requestTime;
+
+  AttendanceDetailVerbose({
+    required this.week,
+    required this.workoutLocation,
+    required this.attendanceStatus,
+    required this.requestDate,
+    required this.requestTime,
+  });
+
+  // ------------------------------------------------------------------------ //
+  //                      JSON SERIALIZATION                                  //
+  // ------------------------------------------------------------------------ //
+  factory AttendanceDetailVerbose.fromJson(Map<String, dynamic> json) => _$AttendanceDetailVerboseFromJson(json);
+}
