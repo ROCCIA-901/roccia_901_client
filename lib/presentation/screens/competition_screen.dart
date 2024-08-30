8d9ee045e1924b03c3b9d957ccfd416373e6af6f
soonvro <soonvro@gmail.com>
2024-08-30 20:03:12 +0900

    fix: 대회 화면 위젯 주석화

--- lib/presentation/screens/competition_screen.dart
+++ lib/presentation/screens/competition_screen.dart
@@ -1,52 +1,52 @@
-import 'package:flutter/material.dart';
-
-import '../../constants/size_config.dart';
-
-class CompetitionScreen extends StatefulWidget {
-  const CompetitionScreen({super.key});
-
-  @override
-  State createState() => _CompetitionScreenState();
-}
-
-class _CompetitionScreenState extends State<CompetitionScreen> {
-  SizeConfig _sizeConfig = SizeConfig();
-
-  @override
-  Widget build(BuildContext context) {
-    _sizeConfig = SizeConfig();
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: EdgeInsets.only(
-                left: _sizeConfig.safeBlockHorizontal * 7,
-                top: _sizeConfig.safeBlockVertical * 3,
-              ),
-              child: Text(
-                '대회',
-                style: TextStyle(
-                  fontSize: _sizeConfig.safeBlockHorizontal * 5.4,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Container(
-              height: _sizeConfig.safeBlockVertical * 70,
-              alignment: Alignment.center,
-              child: Text(
-                "대회 기간에 사용하실 수 있습니다.",
-                style: TextStyle(
-                  color: Color(0xff9a9a9a),
-                  fontSize: _sizeConfig.safeBlockHorizontal * 4,
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// import '../../constants/size_config.dart';
+//
+// class CompetitionScreen extends StatefulWidget {
+//   const CompetitionScreen({super.key});
+//
+//   @override
+//   State createState() => _CompetitionScreenState();
+// }
+//
+// class _CompetitionScreenState extends State<CompetitionScreen> {
+//   SizeConfig _sizeConfig = SizeConfig();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     _sizeConfig = SizeConfig();
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Container(
+//               padding: EdgeInsets.only(
+//                 left: _sizeConfig.safeBlockHorizontal * 7,
+//                 top: _sizeConfig.safeBlockVertical * 3,
+//               ),
+//               child: Text(
+//                 '대회',
+//                 style: TextStyle(
+//                   fontSize: _sizeConfig.safeBlockHorizontal * 5.4,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: _sizeConfig.safeBlockVertical * 70,
+//               alignment: Alignment.center,
+//               child: Text(
+//                 "대회 기간에 사용하실 수 있습니다.",
+//                 style: TextStyle(
+//                   color: Color(0xff9a9a9a),
+//                   fontSize: _sizeConfig.safeBlockHorizontal * 4,
+//                 ),
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
