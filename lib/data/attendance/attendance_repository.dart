8858daa0de8cdb9827f9820d18375ebcf3a72971
soonvro <soonvro@gmail.com>
2024-08-30 20:20:03 +0900

    feat: 출석 상세 API call 추가

--- lib/data/attendance/attendance_repository.dart
+++ lib/data/attendance/attendance_repository.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:untitled/domain/attendance/attendance_detail.dart';
+import 'package:untitled/domain/attendance/attendance_request_list.dart';
 
 import '../../constants/roccia_api.dart';
 import '../../domain/attendance/attendance_dates.dart';
+import '../../domain/attendance/attendance_rate.dart';
+import '../../domain/attendance/user_attendance.dart';
 import '../../utils/app_logger.dart';
 import '../authentication/token_repository.dart';
 import '../shared/api_exception.dart';
@@ -23,6 +27,17 @@ final attendanceRepositoryProvider = Provider<AttendanceRepository>(
   ),
 );
 
+final devAttendanceRepositoryProvider = Provider<AttendanceRepository>(
+  (ref) => AttendanceRepository(
+    api: RocciaApi(),
+    apiClient: RocciaApiClient(
+      interceptors: [
+        RocciaApiLoggerInterceptor(),
+      ],
+    ),
+  ),
+);
+
 class AttendanceRepository {
   final RocciaApi _api;
   final RocciaApiClient _apiClient;
@@ -33,11 +48,102 @@ class AttendanceRepository {
   })  : _api = api,
         _apiClient = apiClient;
 
-  Future<AttendanceDates> getAttendanceDates(int id) async {
+  Future<AttendanceDates> getAttendanceDates() async {
     try {
-      final uri = _api.attendance.dates(id);
+      final uri = _api.attendance.dates();
       final body = await _apiClient.get(uri);
       return AttendanceDates.fromJson(body);
+    } catch (e, stackTrace) {
+      _errorHandler(e, stackTrace);
+      rethrow;
+    }
+  }
+
+  Future<AttendanceDetail> getAttendanceDetail(int userId) async {
+    try {
+      final uri = _api.attendance.detail(userId);
+      final body = await _apiClient.get(uri);
+      return AttendanceDetail.fromJson(body);
+    } catch (e, stackTrace) {
+      _errorHandler(e, stackTrace);
+      rethrow;
+    }
+  }
+
+  Future<AttendanceRate> getAttendanceRate() async {
+    try {
+      final uri = _api.attendance.rate();
+      final body = await _apiClient.get(uri);
+      return AttendanceRate.fromJson(body);
+    } catch (e, stackTrace) {
+      _errorHandler(e, stackTrace);
+      rethrow;
+    }
+  }
+
+  Future<List<AttendanceRequest>> getAttendanceRequests() async {
+    try {
+      final uri = _api.attendance.requests();
+      final body = await _apiClient.get(uri);
+      if (body["X_LIST"] is List<dynamic>) {
+        return List.from(body["X_LIST"].map((req) => AttendanceRequest.fromJson(req)));
+      }
+      throw ApiUnkownException();
+    } catch (e, stackTrace) {
+      _errorHandler(e, stackTrace);
+      rethrow;
+    }
+  }
+
+  Future<void> postRequest() async {
+    try {
+      final uri = _api.attendance.request();
+      final body = await _apiClient.post(uri);
+    } catch (e, stackTrace) {
+      _errorHandler(e, stackTrace);
+      rethrow;
+    }
+  }
+
+  Future<void> patchRequestAccept(int requestId) async {
+    try {
+      final uri = _api.attendance.requestAccept(requestId);
+      final body = await _apiClient.patch(uri);
+    } catch (e, stackTrace) {
+      _errorHandler(e, stackTrace);
+      rethrow;
+    }
+  }
+
+  Future<void> patchRequestReject(int requestId) async {
+    try {
+      final uri = _api.attendance.requestReject(requestId);
+      final body = await _apiClient.patch(uri);
+    } catch (e, stackTrace) {
+      _errorHandler(e, stackTrace);
+      rethrow;
+    }
+  }
+
+  Future<String> getAttendanceLocation() async {
+    try {
+      final uri = _api.attendance.location();
+      final body = await _apiClient.get(uri);
+      return body["workout_location"] ?? "";
+    } catch (e, stackTrace) {
+      _errorHandler(e, stackTrace);
+      rethrow;
+    }
+  }
+
+  Future<List<UserAttendance>> getUserAttendances() async {
+    try {
+      final uri = _api.attendance.users();
+      final body = await _apiClient.get(uri);
+      if (body["X_LIST"] is List<dynamic>) {
+        return List.from(body["X_LIST"].map((user) => UserAttendance.fromJson(user)));
+      }
+      throw ApiUnkownException();
     } catch (e, stackTrace) {
       _errorHandler(e, stackTrace);
       rethrow;
@@ -50,8 +156,7 @@ class AttendanceRepository {
     } on ApiException catch (_) {
       rethrow;
     } catch (e) {
-      logger.w('On Attendance Repo: ${e.toString()}',
-          stackTrace: stackTrace, error: e);
+      logger.w('On Attendance Repo: ${e.toString()}', stackTrace: stackTrace, error: e);
       rethrow;
     }
   }
