90052d8d35a2b271990456ee3154cf1b2ef2f23b
soonvro <soonvro@gmail.com>
2024-08-31 19:20:57 +0900

    fix: 앱 백그라운드 색 변경

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/constants/app_colors.dart';
 
 class AppTheme {
   static ThemeData get lightTheme => ThemeData(
         useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white,
+        splashColor: Colors.transparent,
         colorScheme: ColorScheme.fromSeed(
           seedColor: Color(0xFFCAE4C1),
           primary: Color(0xFFCAE4C1),
